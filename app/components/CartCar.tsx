71e586c2fc3c12fffc4130efcaf1f3d96bd2ad5a
MaXqIvanov <maksivanov35@ya.ru>
2022-07-17 21:58:27 +0300

    update

--- app/components/CartCar.tsx
+++ app/components/CartCar.tsx
@@ -3,58 +3,58 @@ import styles from './CartCar.module.scss';
 export const CartCar = ({elem}:any) => {
   return (
     <div className={`mb-5 col-24 col-lg-12 col-xl-8 ${styles.cart_car}`} key={elem._id}>
-    <div className={styles.car_wrapper}>
-      <div className={styles.description}>{elem.classifieds.description + ' ' + elem.feedData.equipmentVariantName}
-        <span>{elem.feedData.modelYear}</span>
-      </div>
-      <div className={styles.vin}>{elem.vin}</div>
-      <div className={styles.group_img}>
-        {elem.photobank.imgs.map((img:any)=><>
-          <div style={{backgroundImage: `url(${img.url})`}} className={styles.img}>    
-          </div>
-          {elem.photobank.imgs.length == 1 && <div style={{backgroundImage: `url(${img.url})`}}
-          className={styles.img_repeat}></div>}
-        </>)}
-      </div>
-      <div className={styles.info}>
-        <div className={`${styles.group_info}  ${styles.small_group_info}`}>
-          <div className={styles.engine}>
-            <span>Двигатель</span>
-            <div className='d-flex'>{elem.feedData.engine.engineCapacity} л
-            <div className={styles.drop}>/</div>
-            {elem.feedData.engine.enginePower} лс
-            <div className={styles.drop}>/</div>
-            {elem.feedData.engine.engineName.split('-')[1]}
+        <div className={styles.car_wrapper}>
+            <div className={styles.description}>{elem.classifieds.description + ' ' + elem.feedData.equipmentVariantName}
+                <span>{elem.feedData.modelYear}</span>
             </div>
-          </div>
-          <div className={styles.kpp}>
-            <span>Кпп</span>
-            <div>{elem.feedData.equipmentVariantTransmissionType}</div>
-          </div>
+            <div className={styles.vin}>{elem.vin}</div>
+            <div className={styles.group_img}>
+                {elem.photobank.imgs.map((img:any)=><>
+                <div style={{backgroundImage: `url(${img.url})`}} className={styles.img}>    
+                </div>
+                {elem.photobank.imgs.length == 1 && <div style={{backgroundImage: `url(${img.url})`}}
+                className={styles.img_repeat}></div>}
+                </>)}
+            </div>
+            <div className={styles.info}>
+                <div className={`${styles.group_info}  ${styles.small_group_info}`}>
+                <div className={styles.engine}>
+                    <span>Двигатель</span>
+                    <div className='d-flex'>{elem.feedData.engine.engineCapacity} л
+                    <div className={styles.drop}>/</div>
+                    {elem.feedData.engine.enginePower} лс
+                    <div className={styles.drop}>/</div>
+                    {elem.feedData.engine.engineName.split('-')[1]}
+                    </div>
+                </div>
+                <div className={styles.kpp}>
+                    <span>Кпп</span>
+                    <div>{elem.feedData.equipmentVariantTransmissionType}</div>
+                </div>
+                </div>
+                <div className={`${styles.group_info} ${styles.small_group_info} mt-2`}>
+                    <div className={styles.autoProbeg}>
+                    <span>Пробег</span>
+                    <div>{elem.feedData.autoProbeg} км</div>
+                    </div>
+                    <div className={styles.autoColor}>
+                    <span>Цвет</span>
+                    <div>{elem.feedData.colorByClassifierName}</div>
+                    </div>
+                </div>
+                <div className={`${styles.group_info} ${styles.small_group_info} mt-2`}>
+                    <div className={styles.autoPacket}>
+                    <span>Пакеты</span>
+                    <div className={styles.additional_options}>{elem.feedData.baseOptions[0].universalOptions?.uniOptionName}...</div>
+                    <div className={styles.additional}>( + ещё {elem.feedData.baseOptions.length} пакета)</div>
+                    </div>
+                </div>
+                <div className={styles.price}>{elem.feedData.autoPrice.toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2}).replaceAll(',', ' ')}
+                <span>₽</span></div>
+            </div>
+            <div className={elem.setCarStatus.status === 'Продан' ? styles.btn_wait :styles.btn_buy}><span>{elem.setCarStatus.status === 'Продан' ? "В поставке" : "Заказать"}</span></div>
+            <div className={styles.favorite}></div>
         </div>
-        <div className={`${styles.group_info} ${styles.small_group_info} mt-2`}>
-            <div className={styles.autoProbeg}>
-              <span>Пробег</span>
-              <div>{elem.feedData.autoProbeg} км</div>
-            </div>
-            <div className={styles.autoColor}>
-              <span>Цвет</span>
-              <div>{elem.feedData.colorByClassifierName}</div>
-            </div>
-        </div>
-        <div className={`${styles.group_info} ${styles.small_group_info} mt-2`}>
-            <div className={styles.autoPacket}>
-              <span>Пакеты</span>
-              <div className={styles.additional_options}>{elem.feedData.baseOptions[0].universalOptions?.uniOptionName}...</div>
-              <div className={styles.additional}>( + ещё {elem.feedData.baseOptions.length} пакета)</div>
-            </div>
-        </div>
-        <div className={styles.price}>{elem.feedData.autoPrice.toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2}).replaceAll(',', ' ')}
-        <span>₽</span></div>
-      </div>
-      <div className={elem.setCarStatus.status === 'Продан' ? styles.btn_wait :styles.btn_buy}><span>{elem.setCarStatus.status === 'Продан' ? "В поставке" : "Заказать"}</span></div>
-      <div className={styles.favorite}></div>
-    </div>
   </div>
   )
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,11 +2,43 @@ import Link from "next/link";
 import styles from '../components/Header.module.scss';
 const Header = () => {
     return (
-        <div className={`${styles.header} container d-flex justify-content-center align-items-center`}>
-            <div>
-                <Link href={'/3'}>Header</Link>
+    <div className={styles.header}>
+        <nav className="navbar navbar-expand-xl">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Переключатель навигации">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={styles.search}></div>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="#">Все автомобили</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Trade-In</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Что такое Carmart</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">КАСКО</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">FAQ</a>
+                    </li>
+                    <li className={`nav-item ${styles.svg}`}>
+                    <a className={`nav-link ${styles.favorite}`} href="#"></a>
+                    </li>
+                    <li className={`nav-item ${styles.svg}`}>
+                    <a className={`nav-link ${styles.img_car}`} href="#"></a>
+                    </li>
+                </ul>
+                <div className={styles.number}><span>8 812</span>50-11-900</div>
+                <div className={styles.btn}><span>Обратный звонок</span></div>
+                </div>
             </div>
-        </div>
+        </nav>
+    </div>
     );
 }
 
